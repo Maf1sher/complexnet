9301309d9c3fb64901991aa8fa8622229720f21b
Jakub Laskowski <kubas307@gameil.com>
2024-05-30 16:49:26 +0200

    Initial commit

---
+++ src/main/java/com/mafisher/complexnet/ComplexnetApplication.java
@@ -0,0 +1,13 @@
+package com.mafisher.complexnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mafisher/complexnet/ComplexnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafisher.complexnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplexnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
